28ec7ddfe824739c6f7f8afc0f5c68a541a39885
BuzJr <pawlasnr@gmail.com>
2023-02-03 20:26:28 -0500

    Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import cookieParser from 'cookie-parser'
+import multer from 'multer'
+
+const app = express()
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../frontend/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname)
+    }
+  })
+
+const upload = multer({storage})
+
+app.post('/api/upload', upload.single('file'), function (req, res){
+    const file = req.file
+    res.status(200).json(file.filename)
+})
+
+const PORT = 3001;
+
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Connected to ${PORT}!`)
+})
