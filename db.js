ce2fd519bee1ddd75037306808a8f04535b2be2f
BuzJr <pawlasnr@gmail.com>
2023-02-03 20:41:36 -0500

    Fixed for ClearDB

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "BuzzyGok!1",
-    database: "blog"
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b63046b2f41aa5",
+    password: "c10065de",
+    database: "heroku_9d098381e146e66"
 })
 
+
+mysql://b63046b2f41aa5:c10065de@us-cdbr-east-06.cleardb.net/heroku_9d098381e146e66?reconnect=true
