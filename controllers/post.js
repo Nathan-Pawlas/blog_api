f1b2c1eb53b164919bd6a6d039716aa278e0563e
BuzJr <pawlasnr@gmail.com>
2023-02-06 17:17:09 -0500

    l

--- controllers/post.js
+++ controllers/post.js
@@ -43,11 +43,10 @@ export const getPost = (req, res) => {
   };
 
 export const deletePost = (req,res)=> {
-  //CHECK TOKEN
-    const postId = req.params.id
+  
     const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?"
 
-    db.query(q,[postId, userInfo.id], (err,data) => {
+    db.query(q,[postId], (err,data) => {
       if(err) return res.status(403).json("YOU CAN NOT DELETE OTHER'S POSTS")
 
       return res.json("POST DELETED")
