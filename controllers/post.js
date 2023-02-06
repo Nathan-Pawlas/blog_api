8046a09442324e86311f16a6a13be71a799fb0b2
BuzJr <pawlasnr@gmail.com>
2023-02-06 16:18:42 -0500

    fixing sql query

--- controllers/post.js
+++ controllers/post.js
@@ -14,7 +14,7 @@ export const getPosts = (req,res)=> {
 
 export const getPost = (req, res) => {
     const q =
-      "SELECT p.id, `username`, `title`, `desc`, `text`, p.img, u.img AS userImg, `cat`,`date`"
+      "SELECT p.id, `username`,`title`,`desc`,`text`, p.img,`cat`,`date`, FROM posts WHERE p.id = ?"
   
     db.query(q, [req.params.id], (err, data) => {
       if (err) return res.status(500).json(err)
