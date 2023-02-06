c5119bcd3d8f5ecd4f24efef07e3f2497c993f1d
BuzJr <pawlasnr@gmail.com>
2023-02-05 19:51:52 -0500

    Removed cookie check

--- controllers/post.js
+++ controllers/post.js
@@ -24,12 +24,6 @@ export const getPost = (req, res) => {
   }
 
   export const addPost = (req, res) => {
-    const token = req.cookies.access_token;
-    if (!token) return res.status(401).json("Not authenticated!");
-  
-    jwt.verify(token, "jwtkey", (err, userInfo) => {
-      if (err) return res.status(403).json("Token is not valid!");
-  
       const q =
         "INSERT INTO posts(`title`, `desc`, `text`, `img`, `cat`, `date`,`uid`) VALUES (?)";
   
@@ -47,7 +41,6 @@ export const getPost = (req, res) => {
         if (err) return res.status(500).json(err);
         return res.json("Post has been created.");
       });
-    });
   };
 
 export const deletePost = (req,res)=> {
