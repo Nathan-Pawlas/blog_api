67623e83d9310957bb29b3526135d578f905a534
BuzJr <pawlasnr@gmail.com>
2023-02-07 21:22:21 -0500

    abn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/upload')
+    cb(null, '/images')
   },
   filename: function (req, file, cb) {
     cb(null, Date.now()+file.originalname)
