3990169f0540c0909ffa8179ded42dd1d92e55a6
BuzJr <pawlasnr@gmail.com>
2023-02-07 21:07:40 -0500

    ahhh

--- index.js
+++ index.js
@@ -14,22 +14,37 @@ app.use(cors({
   origin: '*'
 }))
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, '/public/upload')
+app.use(bodyparser.json())
+  app.use(bodyparser.urlencoded({
+    extended: true }))
+
+var storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+      callBack(null, './public/images/')     // './public/images/' directory name where save the file
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname))
+  filename: (req, file, callBack) => {
+      callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
   }
 })
 
+var upload = multer({
+  storage: storage})
+
 const upload = multer({storage: storage})
 
-app.post('/api/upload', upload.single('file'), function (req, res){
-  const file = req.file
-  res.status(200).json(file.filename)
-})
-
+app.post("/api/upload", upload.single('file'), (req, res) => {
+  if (!req.file) {
+      console.log("No file upload");
+  } else {
+      console.log(req.file.filename)
+      var imgsrc = 'https://classicsblogapi.herokuapp.com/public/uploads/' + req.file.filename
+      var insertData = "INSERT INTO posts(img)VALUES(?)"
+      db.query(insertData, [imgsrc], (err, result) => {
+          if (err) throw err
+          console.log("file uploaded")
+      })
+  }
+});
 app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
